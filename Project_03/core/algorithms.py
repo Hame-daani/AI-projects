e3ecd27d5e197efd84bc24a6c092711cf9ee6629
Sadegh Hamedani <s.hamedanipr@gmail.com>
2019-11-28 20:25:21 +0330

    cleanup

--- Project_03/core/algorithms.py
+++ Project_03/core/algorithms.py
@@ -8,13 +8,9 @@ def select(population: list, fitness_fn):
     """
     """
     fits = list(map(fitness_fn, population))
-    max_fit = max(fits, key=lambda f: -1 if f == float('inf') else f)
     chances = []
     for fit in fits:
-        if fit == float('inf'):
-            chance = 0
-        else:
-            chance = (max_fit-fit)+1
+        chance = fit
         if chances:
             chances.append(chance+chances[-1])
         else:
@@ -30,24 +26,26 @@ def reproduce(x: list, y: list):
     """
     """
     n = len(x)
-    c = random.randrange(0, n)
+    c = random.randint(0, n-1)
     child = x[:c] + y[c:]
     return child
 
 
 def mutate(child, gene_pool: list):
     n = len(child)
-    g = len(gene_pool)
-    c = random.randrange(0, n)
-    r = random.randrange(0, g)
-    # random.choice(gene_pool)
-    new_gene = gene_pool[r]
+    c = random.randint(0, n-1)
+    new_gene = random.choice(gene_pool)
     return child[:c] + [new_gene] + child[c + 1:]
 
 
-def genetic_algorithm(population: list, gene_pool: list, fitness_fn, mutate_probability=0.1, fit_target=0, time_target=0, num_gen=1000):
+def genetic_algorithm(problem: GeneticProblem, population: list = None, mutate_probability=0.1, fit_target=0, time_target=0, num_gen=1000):
     """
     """
+    all_best = float('inf')
+    if not population:
+        population = problem.population
+    fitness_fn = problem.fitness_fn
+    gene_pool = problem.gene_pool
     start_time = time.time()
     num_generation = 0
 
@@ -73,15 +71,17 @@ def genetic_algorithm(population: list, gene_pool: list, fitness_fn, mutate_prob
                 child = mutate(child, gene_pool)
             new_population.append(child)
         population = new_population
-        best = min(population, key=fitness_fn)
+        best = max(population, key=fitness_fn)
+        best_fit = problem.longest - fitness_fn(best)
+        all_best = best_fit if all_best > best_fit else all_best
         print(
-            f"Generation {num_generation}: {best} -> {fitness_fn(best)}", end='\r'
+            f"best: {all_best} - Generation {num_generation}: {best_fit} -> {best[:20]}", end='\r'
         )
-    return {'population': population, 'best': min(population, key=fitness_fn), 'weight': fitness_fn(min(population, key=fitness_fn))}
+    return {'population': population, 'best': best, 'weight': best_fit}
 
 
 def genetic_serach(problem: GeneticProblem, population=None):
     if not population:
-        return genetic_algorithm(population=problem.population, gene_pool=problem.gene_pool, fitness_fn=problem.fitness_fn)
+        return genetic_algorithm(problem=problem, population=problem.population, gene_pool=problem.gene_pool, fitness_fn=problem.fitness_fn)
     else:
         return genetic_algorithm(population=population, gene_pool=problem.gene_pool, fitness_fn=problem.fitness_fn)
