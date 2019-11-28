a58745286480807282a2dde6dbf8cba48bf3d04a
Sadegh Hamedani <s.hamedanipr@gmail.com>
2019-11-28 15:38:27 +0330

    add number of generations

--- Project_03/core/algorithms.py
+++ Project_03/core/algorithms.py
@@ -45,20 +45,25 @@ def mutate(child, gene_pool: list):
     return child[:c] + [new_gene] + child[c + 1:]
 
 
-def genetic_algorithm(population: list, gene_pool: list, fitness_fn, mutate_probability=0.1, fit_target=0, time_target=3):
+def genetic_algorithm(population: list, gene_pool: list, fitness_fn, mutate_probability=0.1, fit_target=0, time_target=0, num_gen=1000):
     """
     """
     start_time = time.time()
+    num_generation = 0
 
     def finished():
-        if not fit_target:
+        if time_target:
             if time.time()-start_time >= time_target:
                 return True
-        else:
+        elif fit_target:
             pass
+        elif num_gen:
+            if num_generation > num_gen:
+                return True
         return False
     # main func
     while(not finished()):
+        num_generation += 1
         new_population = []
         for i in range(len(population)):
             x, y = select(population, fitness_fn)
@@ -68,10 +73,14 @@ def genetic_algorithm(population: list, gene_pool: list, fitness_fn, mutate_prob
                 child = mutate(child, gene_pool)
             new_population.append(child)
         population = new_population
+        best = min(population, key=fitness_fn)
+        print(
+            f"Generation {num_generation}: {best} -> {fitness_fn(best)}", end='\r'
+        )
     return {'population': population, 'best': min(population, key=fitness_fn), 'weight': fitness_fn(min(population, key=fitness_fn))}
 
 
-def genetic_serach(problem: GeneticProblem,population=None):
+def genetic_serach(problem: GeneticProblem, population=None):
     if not population:
         return genetic_algorithm(population=problem.population, gene_pool=problem.gene_pool, fitness_fn=problem.fitness_fn)
     else:
